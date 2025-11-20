6506ffa2a4dc3a5df646ea9b30f451869825ce70
benjuwan <benjuwan.banquet12@gmail.com>
2025-11-20 15:07:02 +0900

    fix: 各種ライブラリのアップデート・アップグレード及びREADMEの更新、リファクタリング.

--- src/components/korekau/utils/KorekauItemEditer.tsx
+++ src/components/korekau/utils/KorekauItemEditer.tsx
@@ -13,7 +13,7 @@ export const KorekauItemEditer = memo(({ props }: { props: itemEditerType }) => 
     const { classNameStr, category, korekauList } = props;
 
     return (
-        <div className={classNameStr}>
+        <div className={`${classNameStr} overflow-y-auto`}>
             <div className="p-[1em] shadow-[0_0_8px_rgba(0,0,0,.25)_inset] bg-white mb-[1em] rounded flex flex-row flex-wrap items-center gap-[1em]">
                 <KorekauItemIcons category={category} />
                 <p className="w-[80%] wrap-anywhere">{korekauList.itemName}<span className="mx-[1em] text-[#59b835]">×{korekauList.itemNumber}</span>の内容を編集</p>
